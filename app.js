7f913a1568c629be5eb516938e5dadb96de7757f
margarita-salazar <margarita.salazar.mls@gmail.com>
2022-02-25 12:14:44 -0600

    added controls for the keys on the keyboard

--- app.js
+++ app.js
@@ -68,16 +68,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  timerId = setInterval(moveDown, 1000);
-
   function freeze() {
     if (
-      current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))
+      current.some((index) => squares[currentPosition + index + width].classList.contains("taken"))
     ) {
-      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      current.forEach((index) => squares[currentPosition + index].classList.add("taken"));
+
       random = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
+
       draw();
     }
   }
@@ -89,4 +89,60 @@ document.addEventListener("DOMContentLoaded", () => {
     freeze();
   }
 
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === 0);
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some((index) => squares[currentPosition + index].classList.contains("taken"))
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some((index) => (currentPosition + index) % width === width - 1);
+
+    if (!isAtRightEdge) currentPosition += 1;
+
+    if (current.some((index) => squares[currentPosition + index].classList.contains("taken"))) {
+      currentPosition -= 1;
+    }
+
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    // eslint-disable-next-line no-plusplus
+    currentRotation++;
+
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+
+    current = theTetrominoes[random][currentRotation];
+
+    draw();
+  }
+
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+
+  document.addEventListener("keyup", control);
+
+  timerId = setInterval(moveDown, 1000);
 });
