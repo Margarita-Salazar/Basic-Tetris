caf9f89c725452d19c23fbb7ba209aa3efaf0af3
margarita-salazar <margarita.salazar.mls@gmail.com>
2022-02-24 21:47:46 -0600

    shapes can move down and stay down if reached the bottom

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
   const ScoreDisplay = document.querySelector("#score");
@@ -40,4 +40,53 @@ document.addEventListener("DOMContentLoded", () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+
+  let current = theTetrominoes[random][currentRotation];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  }
+
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino");
+    });
+  }
+
+  timerId = setInterval(moveDown, 1000);
+
+  function freeze() {
+    if (
+      current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))
+    ) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
+
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
 });
